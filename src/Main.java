b3525c8bdb6d2e4b625c5685a50e14135891b992
Tunnikov Artur <tunnikovartur@gmail.com>
2022-10-28 09:23:24 +0300

    Add Main file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Vector<Double[][]> matrix_A = new Vector();
+        System.out.println("Hello world!");
+    }
+}
